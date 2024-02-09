351787ca86e5c1e6656a85addd1028d140f7b632
Serelik <zdivanal@gmail.com>
2024-02-09 18:34:17 +0100

    async and fix model

--- app/src/main/java/com/serelik/movieapp/ui/actorDetails/ActorDetailsViewModel.kt
+++ app/src/main/java/com/serelik/movieapp/ui/actorDetails/ActorDetailsViewModel.kt
@@ -1,5 +1,6 @@
 package com.serelik.movieapp.ui.actorDetails
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,6 +12,7 @@ import com.serelik.movieapp.data.network.ActorDetailsMapper
 import com.serelik.movieapp.data.network.MovieDBApi
 import com.serelik.movieapp.data.network.MoviesByActorMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -28,11 +30,17 @@ class ActorDetailsViewModel @Inject constructor(
         mutableLiveData
 
     fun getMovieAndActorInfo(id: Int) {
+
+        val moviesResponseAsync = viewModelScope.async { movieApiService.getMoviesByActor(id) }
+        val actorInfoResponseAsync = viewModelScope.async { movieApiService.getActor(id) }
+
+
         viewModelScope.launch {
             try {
                 mutableLiveData.postValue(LoadingResults.Loading)
-                val moviesResponse = movieApiService.getMoviesByActor(id)
-                val actorInfoResponse = movieApiService.getActor(id)
+
+                val moviesResponse = moviesResponseAsync.await()
+                val actorInfoResponse = actorInfoResponseAsync.await()
 
                 val actor = actorDetailsMapper.parseActorDetailsResponse(actorInfoResponse)
 
@@ -41,8 +49,11 @@ class ActorDetailsViewModel @Inject constructor(
 
                 mutableLiveData.postValue(LoadingResults.Success(Pair(actor, movies)))
             } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e(this::class.simpleName, e.message.toString())
                 mutableLiveData.postValue(LoadingResults.Error(e))
             }
         }
     }
+
 }
