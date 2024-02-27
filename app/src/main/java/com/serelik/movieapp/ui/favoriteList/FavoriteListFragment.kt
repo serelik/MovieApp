56ac64ac8374a350ad08dde3311c1dfcc93130f9
Serelik <zdivanal@gmail.com>
2024-02-27 02:06:22 +0100

    fix favorite transition to details

--- app/src/main/java/com/serelik/movieapp/ui/favoriteList/FavoriteListFragment.kt
+++ app/src/main/java/com/serelik/movieapp/ui/favoriteList/FavoriteListFragment.kt
@@ -9,7 +9,6 @@ import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.serelik.movieapp.R
 import com.serelik.movieapp.databinding.FragmentFavoriteListBinding
-import com.serelik.movieapp.ui.movieList.MovieListFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -48,7 +47,7 @@ class FavoriteListFragment : Fragment(R.layout.fragment_favorite_list) {
     private fun onMovieClick(movieId: Int) {
         val controller = findNavController()
         controller.navigate(
-            MovieListFragmentDirections.actionMovieListFragmentToMovieDetailsFragment(
+            FavoriteListFragmentDirections.actionFavoriteListFragmentToMovieDetailsFragment(
                 movieId
             )
         )
